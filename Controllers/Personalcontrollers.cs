361071d441ff985891c86f4eeb8a5962786b800e
WilberJose <wilberjose200208@gmail.com>
2021-11-05 09:15:04 -0600

    ejemplo MVC

---
+++ Controllers/Personalcontrollers.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using firstApplication.Models;
+
+namespace firstApplication.Controllers
+{
+    public class PersonalController : Controller
+    { 
+        public IActionResult Index()
+        {
+            Personal personal = new Personal();
+            personal.name = "Carmen";
+            personal.lastname = "Flores"; 
+            personal.age ="30";
+            return View(personal);
+        }
+
+    }
+}
+    
---
+++ Models/Personal.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace firstApplication.Models
+{
+    public class Personal 
+    {
+        public string name; 
+
+        public string lastname;
+
+        public string age; 
+    }
+}
